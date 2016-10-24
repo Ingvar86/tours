c7e88cbee50ba21c50abbbd6607a17db34788cb5
Ingvar86 <gara314@gmail.com>
2016-10-24 17:25:23 +0300

    Add server

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict'
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + 'public'));
+
+app.post('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
